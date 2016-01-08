13d60ded4525e7aebfbcc61990497598bc04ebc7
Robert Sawko <r.sawko@cranfield.ac.uk>
2016-01-08 12:22:14 +0000

    Add constant translation model

---
+++ src/constantTranslation.C
@@ -0,0 +1,56 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "constantTranslation.H"
+#include "fvCFD.H"
+
+#include "addToRunTimeSelectionTable.H"
+
+namespace Foam {
+defineTypeNameAndDebug(constantTranslation, 0);
+
+addToRunTimeSelectionTable(translationalFrame, constantTranslation, dictionary);
+}
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+//
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::constantTranslation::constantTranslation(const fvMesh &mesh,
+                                               const IOdictionary &dict)
+    : translationalFrame(mesh, dict) {
+    VF_ = dict_.subDict(typeName + "Coeffs").lookup("velocity");
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+vector
+Foam::constantTranslation::calculate_acceleration(const volScalarField &p,
+                                                  const volSymmTensorField &R) {
+    return vector(0, 0, 0);
+}
+
+// ************************************************************************* //
